b848230164f42f9845e6e0c29353e52efb4e9f9d
qayamuddink <qayamuddin1k@gmail.com>
2024-08-12 12:08:47 +0530

    page 4 work

--- index.js
+++ index.js
@@ -11,23 +11,16 @@ tl.from( ".line h1", {
 tl.from("#line1-part1" ,{
     opacity:0,
     onStart: function(){
-
         var h5timer = document.querySelector("#line1-part1 h5");
         var grow = 0 ;
         setInterval(function() {
             if(grow < 100){
-                h5timer.innerHTML = grow++;
-               
+                h5timer.innerHTML = grow++;  
             }
             else{
-                
                 h5timer.innerHTML = grow
-            }
-           
-            
-        },40);
-        
-        
+            }            
+        },27);
     }
 })
 tl.to(".line h2",{
@@ -37,15 +30,14 @@ tl.to(".line h2",{
 tl.to("#loader" ,{
     opacity : 0 ,
     duration: 0.2,
-    delay: 0
+    delay: 2.6
 })
 
 tl.from("#page1", {
-    delay: 0.1 ,
-    y: 1600 ,
-    opacity:0,
-    duration:0.6,
-    ease: Power4.easeOut 
+    delay: 0.1,
+    y: 1600,
+    duration: 0.5,
+    ease: Power4,
 })
 tl.to("#loader" , {
     display :"none"
@@ -54,9 +46,10 @@ tl.from("#nav" ,{
     opacity:0
 })
 tl.from("#hero1 h1 ,#hero2 h1,#hero3 h2 ,#hero4 h1"  ,{
-    y:120 ,
+    y:140 ,
     stagger:0.2
 })
+
 }
 
 function cursorAnimation(){
