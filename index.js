ec87ba8e2ab8b04f50cabe20c8ecaf38d05bc42b
qayamuddink <qayamuddin1k@gmail.com>
2024-08-12 09:16:31 +0530

    page1 done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+function loadingAnimation(){
+    
 var tl = gsap.timeline()
 tl.from( ".line h1", {
     y:150,
@@ -36,16 +37,45 @@ tl.to(".line h2",{
 tl.to("#loader" ,{
     opacity : 0 ,
     duration: 0.2,
-    delay: 4
+    delay: 0
 })
 
 tl.from("#page1", {
     delay: 0.1 ,
     y: 1600 ,
     opacity:0,
-    duration:0.5,
-    ease:power4
+    duration:0.6,
+    ease: Power4.easeOut 
 })
 tl.to("#loader" , {
     display :"none"
 })
+tl.from("#nav" ,{
+    opacity:0
+})
+tl.from("#hero1 h1 ,#hero2 h1,#hero3 h2 ,#hero4 h1"  ,{
+    y:120 ,
+    stagger:0.2
+})
+}
+
+function cursorAnimation(){
+
+    document.addEventListener("mousemove" ,function(dets){
+        gsap.to("#crsr", {
+            left:dets.x ,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4" , 
+        //Parameters are optional.
+        // ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        // duration: 1,
+      );
+
+}
+
+loadingAnimation()
+cursorAnimation();
+
